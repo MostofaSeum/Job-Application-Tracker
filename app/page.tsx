b5eb2ce88f1e428be88e270404fdd553a150d007
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-11 10:06:32 +0600

    feat: initialize project with Tailwind CSS and shadcn/ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -6,9 +7,13 @@ export default function Home() {
       <main className="flex 1">
         {/* Hero Section */}
         <section className="container mx-auto px-4 py-32">
-          <div>
-            <h1 className="text-black">A Better Way to track your job application</h1>
-            <p className="text-muted-foreground"> Capture, organize and manage your job search in one place </p>
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold">A Better Way to track your job application</h1>
+            <p className="text-muted-foreground mb-10 text-xl"> Capture, organize and manage your job search in one place </p>
+            <div className="flex flex-col items-center gap-4">
+              <Button>Start For Free</Button>
+              <p>Free forever, No credit card required</p>
+            </div>
           </div>
         </section>
       </main>
