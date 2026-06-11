a53eb28a5a16ad26d3e99e2bebc66f8a8e8ffb73
MostofaSeum <mostofa.seum@brainicontech.com>
2026-06-11 09:26:51 +0600

    feat: initialize landing page with hero section structure

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col"></div>
+    <div className="flex min-h-screen flex-col bg-white ">
+      <main className="flex 1">
+        {/* Hero Section */}
+        <section className="text-black">
+          <h1>A Better Way to track your job application</h1>
+        </section>
+      </main>
+    </div>
   );
 }
